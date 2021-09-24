bd08a32788f3f539a2ed68977874a52f9abdb007
Carson <cpsievert1@gmail.com>
2021-09-24 14:57:28 -0500

    Use pytest-snapshot for non-trivial html output tests

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,70 +1,48 @@
 from htmltools import *
+import json
 
-def expect_resolved_deps(input, output):
-  actual = tag_list(*input).render()['dependencies']
-  assert actual == output
 
-def expect_html_deps(x, html_, deps):
-  assert str(x) == html_
-  assert x.render()['dependencies'] == deps
+def test_dep_resolution(snapshot):
+  a1_1 = html_dependency("a", "1.1", {"href": "/"}, script="a1.js")
+  a1_2 = html_dependency("a", "1.2", {"href": "/"}, script="a2.js")
+  a1_2_1 = html_dependency("a", "1.2.1", {"href": "/"}, script="a3.js")
+  b1_0_0 = html_dependency("b", "1.0.0", {"href":"/"}, script="b1.js")
+  b1_0_1 = html_dependency("b", "1.0.1", {"href":"/"}, script="b2.js")
+  c1_0 = html_dependency("c", "1.0", {"href":"/"}, script="c1.js")
+  test = tag_list(*[a1_1, b1_0_0, b1_0_1, a1_2, a1_2_1, b1_0_0, b1_0_1, c1_0])
+  snapshot.assert_match(test.render()['html'], "dep_resolution.txt")
 
-def test_dep_resolution():
-  a1_1 = html_dependency("a", "1.1", {"href":"/"})
-  a1_2 = html_dependency("a", "1.2", {"href":"/"})
-  a1_2_1 = html_dependency("a", "1.2.1", {"href":"/"})
-  b1_0_0 = html_dependency("b", "1.0.0", {"href":"/"})
-  b1_0_1 = html_dependency("b", "1.0.1", {"href":"/"})
-  c1_0 = html_dependency("c", "1.0", {"href":"/"})
-  expect_resolved_deps(
-    [a1_1, b1_0_0, b1_0_1, a1_2, a1_2_1, b1_0_0, b1_0_1, c1_0],
-    [a1_2_1, b1_0_1, c1_0]
-  )
+# Test out renderTags and findDependencies when tags are inline
+def test_inline_deps(snapshot):
+  a1_1 = html_dependency("a", "1.1", {"href":"/"}, script="a1.js")
+  a1_2 = html_dependency("a", "1.2", {"href": "/"}, script="a2.js")
+  tests = [
+      tag_list(a1_1, div("foo"), "bar"),
+      tag_list(a1_1, div("foo"), a1_2, "bar"),
+      div(a1_1, div("foo"), "bar"),
+      tag_list([a1_1, div("foo")], "bar"),
+      div([a1_1, div("foo")], "bar")
+  ]
+  html_ = "\n\n".join([t.render()['html'] for t in tests])
+  snapshot.assert_match(html_, "inline_deps.txt")
 
-def test_inline_deps():
-  # Test out renderTags and findDependencies when tags are inline
-  a1_1 = html_dependency("a", "1.1", {"href":"/"})
-  a1_2 = html_dependency("a", "1.2", {"href":"/"})
-  # tagLists ----------------------------------------------------------
-  expect_html_deps(
-    tag_list(a1_1, div("foo"), "bar"), 
-    "<div>foo</div>\nbar", [a1_1]
-  )
-  expect_html_deps(
-    tag_list(a1_1, div("foo"), a1_2, "bar"), 
-    "<div>foo</div>\nbar", [a1_2]
-  )
-  # tags with children ------------------------------------------------
-  expect_html_deps(
-    div(a1_1, div("foo"), "bar"), 
-    "<div>\n  <div>foo</div>\n  bar\n</div>", [a1_1]
-  )
-  # Passing normal lists to tagLists and tag functions  ---------------
-  expect_html_deps(
-    tag_list([a1_1, div("foo")], "bar"), 
-    "<div>foo</div>\nbar", [a1_1]
-  )
-  expect_html_deps(
-    div([a1_1, div("foo")], "bar"), 
-    "<div>\n  <div>foo</div>\n  bar\n</div>", [a1_1]
-  )
-
-def test_append_deps():
-  a1_1 = html_dependency("a", "1.1", {"href":"/"})
-  a1_2 = html_dependency("a", "1.2", {"href":"/"})
-  b1_2 = html_dependency("b", "1.0", {"href":"/"})
+def test_append_deps(snapshot):
+  a1_1 = html_dependency("a", "1.1", {"href":"/"}, script="a1.js")
+  a1_2 = html_dependency("a", "1.2", {"href":"/"}, script="a2.js")
+  b1_2 = html_dependency("b", "1.0", {"href":"/"}, script="b1.js")
   x = div(a1_1, b1_2)
   x.append(a1_2)
-  expect_html_deps(x, "<div></div>", [a1_2, b1_2])
-  x = div(a1_1)
-  x.append([a1_2, b1_2])
-  expect_html_deps(x, "<div></div>", [a1_2, b1_2])
-  x = div()
-  x.append([a1_1, b1_2])
-  x.append(a1_2)
-  expect_html_deps(x, "<div></div>", [a1_2, b1_2])
+  y = div(a1_1)
+  y.append([a1_2, b1_2])
+  z = div()
+  z.append([a1_1, b1_2])
+  z.append(a1_2)
+  tests = [x, y, z]
+  html_ = "\n\n".join([t.render()['html'] for t in tests])
+  snapshot.assert_match(html_, "append_deps.txt")
 
 
-def test_script_input():
+def test_script_input(snapshot):
   def fake_dep(**kwargs):
     return html_dependency("a", "1.0", "srcpath", **kwargs)
   dep1 = fake_dep(script = "js/foo bar.js", stylesheet = "css/bar foo.css")
@@ -72,5 +50,6 @@ def test_script_input():
   dep3 = fake_dep(script = [{"src": "js/foo bar.js"}], stylesheet = [{"href": "css/bar foo.css"}])
   assert dep1 == dep2 == dep3
   # Make sure repeated calls to as_html() repeatedly encode
+  test = tag_list([dep1, dep2, dep3])
   for i in range(2):
-    assert str(dep1) == '<link href="srcpath/css/bar%20foo.css" rel="stylesheet"/>\n<script src="srcpath/js/foo%20bar.js"></script>'
+    snapshot.assert_match(test.render()['html'], "script_input.txt")
