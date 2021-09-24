6251266ae72ffaec943cd5e0686d4f57c4d1a08f
Carson <cpsievert1@gmail.com>
2021-09-24 15:33:25 -0500

    Add some JSX tests and make a fixes for (1) jsx() on an attribute and (2) html attribute writing within JSX tag

--- tests/test_jsx_tags.py
+++ tests/test_jsx_tags.py
@@ -1,9 +1,28 @@
 from htmltools import *
 
-def expect_html(tag: tag, expected: str):
-  assert str(tag) == expected
-
-# TODO: add more tests! Also why is this not working?
-#def test_jsx_tags():
-#  x = jsx_tag("MyComponent")(span(), foo=jsx_tag("Foo")(), foo2=[div(), div()], bar=1, baz=jsx("foo"), style={'margin': '1rem'})
-#  expect_html(x, '<script type="text/javascript">\n(function() {\n  var container = new DocumentFragment();\n  ReactDOM.render(React.createElement(MyComponent, {foo: "React.createElement(Foo, null)", foo2: "[React.createElement(\'div\', null), React.createElement(\'div\', null)]", bar: "1", baz: "foo", style: "{margin: "1rem"}", }, React.createElement(\'span\', null)), container);\n  document.currentScript.after(container);\n})();\n</script>')
+def test_jsx_tags(snapshot):
+  Foo = jsx_tag("Foo")
+  snapshot.assert_match(Foo().render()['html'], "jsx_single.txt")
+  Bar = jsx_tag("Bar")
+  # Only the "top-level" tag gets wrapped in <script> tags
+  snapshot.assert_match(Foo(Bar()).render()['html'], "jsx_nested.txt")
+  x = Foo(
+    span(), 
+    "childtext",
+    jsx("`childexpression`"),
+    Foo(),
+    [Foo(), Bar()],
+    tag_list(Foo(), Bar()),
+    int=1,
+    float=2.0,
+    bool=True,
+    null=None,
+    string="string",
+    list=[1, 2, 3],
+    dict={"a": 1, "b": 2},
+    jsxTag=Bar(),
+    htmlTag=[div(), div(foo=1)],
+    baz=jsx("foo"), 
+    style={'margin': '1rem'}
+  )
+  snapshot.assert_match(str(x), "jsx.txt")
