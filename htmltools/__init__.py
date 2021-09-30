bd364be873614c492824f4ebcd44c318f815d6e8
Winston Chang <winston@stdout.org>
2021-09-30 15:29:28 -0500

    Rename tags2 to tags

--- htmltools/__init__.py
+++ htmltools/__init__.py
@@ -1,3 +1,3 @@
-from .tags import *
-from .tags2 import p, h1, h2, h3, h4, h5, h6, a, br, div, span, pre, code, img, strong, em, hr
+from .core import *
+from .tags import p, h1, h2, h3, h4, h5, h6, a, br, div, span, pre, code, img, strong, em, hr
 from .util import css
