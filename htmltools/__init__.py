f8194d0890f6c03bd096deeeb0c38af10dddc70a
Winston Chang <winston@stdout.org>
2022-06-30 16:35:09 -0500

    Use explicit imports in __init__.py

--- htmltools/__init__.py
+++ htmltools/__init__.py
@@ -1,14 +1,80 @@
 __version__ = "0.1.0.9001"
 
-from ._core import *
-from .tags import *
+from ._core import (
+    TagList,
+    Tag,
+    HTMLDocument,
+    HTML,
+    MetadataNode,
+    HTMLDependency,
+    RenderedHTML,
+    TagAttrArg,
+    TagChildArg,
+    TagChild,
+    TagFunction,
+    Tagifiable,
+    head_content,
+)
+from ._jsx import jsx, jsx_tag_create, JSXTag, JSXTagAttrArg
+from ._util import css
+from .tags import (
+    p,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    a,
+    br,
+    div,
+    span,
+    pre,
+    code,
+    img,
+    strong,
+    em,
+    hr,
+)
+from . import tags
 from . import svg
-from ._util import *
-from ._jsx import *
 
-from ._core import __all__ as _core_all
-from .tags import __all__ as _tags_all
-from ._util import __all__ as _util_all
-from ._jsx import __all__ as _jsx_all
-
-__all__ = _core_all + _tags_all + _util_all + _jsx_all + ("tags", "svg")
+__all__ = (
+    "TagList",
+    "Tag",
+    "HTMLDocument",
+    "HTML",
+    "MetadataNode",
+    "HTMLDependency",
+    "RenderedHTML",
+    "TagAttrArg",
+    "TagChildArg",
+    "TagChild",
+    "TagFunction",
+    "Tagifiable",
+    "head_content",
+    "jsx",
+    "jsx_tag_create",
+    "JSXTag",
+    "JSXTagAttrArg",
+    "css",
+    "p",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "a",
+    "br",
+    "div",
+    "span",
+    "pre",
+    "code",
+    "img",
+    "strong",
+    "em",
+    "hr",
+    "tags",
+    "svg",
+)
